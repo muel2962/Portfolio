4e532dd254a0da9b65aa491b161a0a17b291c783
Muel <ssammootv153@gmail.com>
2026-03-15 19:26:36 +0900

    Add files via upload

--- muel/static/script.js
+++ muel/static/script.js
@@ -1,3 +1,15 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+document.addEventListener('dragstart', event => event.preventDefault());
+
+document.addEventListener('selectstart', event => event.preventDefault());
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'F12' || (event.ctrlKey && ['c', 'v', 'u', 's', 'p'].includes(event.key.toLowerCase()))) {
+        event.preventDefault();
+    }
+});
+
 const btnLight = document.getElementById('btn-light');
 const btnGithub = document.getElementById('btn-github');
 const btnDiscord = document.getElementById('btn-discord');
@@ -19,6 +31,14 @@ document.addEventListener('mousemove', (e) => {
   if (isFlashlightOn) {
     const x = e.clientX;
     const y = e.clientY;
+    flashlightOverlay.style.background = `radial-gradient(circle 150px at ${x}px ${y}px, transparent 0%, rgba(0, 0, 0, 1) 100%)`;
+  }
+});
+
+document.addEventListener('touchmove', (e) => {
+  if (isFlashlightOn && e.touches.length > 0) {
+    const x = e.touches[0].clientX;
+    const y = e.touches[0].clientY;
     flashlightOverlay.style.background = `radial-gradient(circle 150px at ${x}px ${y}px, transparent 0%, rgba(0, 0, 0, 1) 100%)`;
   }
 });
